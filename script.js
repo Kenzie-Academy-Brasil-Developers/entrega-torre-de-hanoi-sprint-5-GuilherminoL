c2b558e3030a0b1a8a0107d065860cf22a129fbc
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-08-03 08:01:38 -0300

    feat:Adicionando movimento invalido

--- script.js
+++ script.js
@@ -68,8 +68,11 @@ const getDiscOut = element => {
         holder.appendChild(disc)
         moveCount++
     }
-    else {
+    else if (verifyMovement(element)){
         element.currentTarget.appendChild(holder.lastElementChild)
+    }
+    else{
+        win.innerHTML = 'Invalid Move!!'
     }
     
 }
